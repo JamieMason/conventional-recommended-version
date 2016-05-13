f78d63a3f5a8335346db8f4575a71d0f71e08983
Jamie Mason <jmason@williamhill.co.uk>
2016-05-13 08:18:56 +0100

    style(format): add semistandard formatter

--- cli.js
+++ cli.js
@@ -7,19 +7,19 @@ var pkg = require('./package.json');
 var crv = require('./index');
 
 program
-    .version(pkg.version)
-    .option('-d, --directory [location]', 'path to local git repository', process.cwd())
-    .option('-p, --postfix [name]', 'a postfix such as "rc1", "canary" or "beta1"', '')
-    .parse(process.argv);
+  .version(pkg.version)
+  .option('-d, --directory [location]', 'path to local git repository', process.cwd())
+  .option('-p, --postfix [name]', 'a postfix such as "rc1", "canary" or "beta1"', '')
+  .parse(process.argv);
 
 crv.get({
-    directory: program.directory,
-    postfix: program.postfix
+  directory: program.directory,
+  postfix: program.postfix
 }, onVersion);
 
-function onVersion(error, version) {
-    if (error) {
-        throw error;
-    }
-    console.log(version);
+function onVersion (error, version) {
+  if (error) {
+    throw error;
+  }
+  console.log(version);
 }
--- index.js
+++ index.js
@@ -3,81 +3,81 @@
 var gitlog = require('gitlog');
 
 module.exports = {
-    get: get
+  get: get
 };
 
-function get(options, done) {
-    var postfix = getPostfix(options.postfix);
-    var directory = options.directory;
+function get (options, done) {
+  var postfix = getPostfix(options.postfix);
+  var directory = options.directory;
 
-    var breaking = 0;
-    var feat = 0;
-    var fix = 0;
+  var breaking = 0;
+  var feat = 0;
+  var fix = 0;
 
-    gitlog({
-        fields: ['subject', 'body'],
-        number: 9000 * 1000 * 1000,
-        repo: directory
-    }, parseGitHistory);
+  gitlog({
+    fields: ['subject', 'body'],
+    number: 9000 * 1000 * 1000,
+    repo: directory
+  }, parseGitHistory);
 
-    function parseGitHistory(error, commits) {
-        if (!error) {
-            commits.reverse().forEach(parseCommit);
-            done(null, breaking + '.' + feat + '.' + fix + postfix);
-        } else {
-            done(error);
-        }
+  function parseGitHistory (error, commits) {
+    if (!error) {
+      commits.reverse().forEach(parseCommit);
+      done(null, breaking + '.' + feat + '.' + fix + postfix);
+    } else {
+      done(error);
     }
+  }
 
-    function parseCommit(commit) {
-      return parseMessage(commit.body.trim());
-    }
+  function parseCommit (commit) {
+    return parseMessage(commit.body.trim());
+  }
 
-    function parseMessage(message) {
-        if (isRelease(message)) {
-            var release = parseReleaseCommit(message);
-            breaking = release.major;
-            feat = release.minor;
-            fix = release.patch;
-        } else if (isBreakingChange(message)) {
-            breaking++;
-            feat = 0;
-            fix = 0;
-        } else if (isFeature(message)) {
-            feat++;
-            fix = 0;
-        } else if (isFix(message)) {
-            fix++;
-        }
+  function parseMessage (message) {
+    if (isRelease(message)) {
+      var release = parseReleaseCommit(message);
+      breaking = release.major;
+      feat = release.minor;
+      fix = release.patch;
+    } else if (isBreakingChange(message)) {
+      breaking++;
+      feat = 0;
+      fix = 0;
+    } else if (isFeature(message)) {
+      feat++;
+      fix = 0;
+    } else if (isFix(message)) {
+      fix++;
     }
+  }
 
-    function isRelease(message) {
-        return message.startsWith('chore(release)');
-    }
+  function isRelease (message) {
+    return message.startsWith('chore(release)');
+  }
 
-    function isFeature(message) {
-        return message.startsWith('feat(');
-    }
+  function isFeature (message) {
+    return message.startsWith('feat(');
+  }
 
-    function isFix(message) {
-        return message.startsWith('fix(');
-    }
+  function isFix (message) {
+    return message.startsWith('fix(');
+  }
 
-    function isBreakingChange(message) {
-        return message.search(/BREAKING CHANGE/) !== -1;
-    }
+  function isBreakingChange (message) {
+    return message.search(/BREAKING CHANGE/) !== -1;
+  }
 
-    function parseReleaseCommit(message) {
-        var parts = message.split(/[ \.-]/);
-        return {
-            major: parseInt(parts[1], 10),
-            minor: parseInt(parts[2], 10),
-            patch: parseInt(parts[3], 10),
-            postfix: getPostfix(parts[4])
-        };
-    }
+  function parseReleaseCommit (message) {
+    var parts = message.split(/[ \.-]/);
+    return {
+      major: parseInt(parts[1], 10),
+      minor: parseInt(parts[2], 10),
+      patch: parseInt(parts[3], 10),
+      postfix: getPostfix(parts[4])
+    };
+  }
 
-    function getPostfix(value) {
-        return value ? '-' + value : '';
-    }
+  function getPostfix (value) {
+    return value ? '-' + value : '';
+  }
 }
